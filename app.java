f9a8ce37f6fc56e1957a8c67a04bbb77af053c9b
saikrishna123-del <only.saikrishna1@gmail.com>
2025-08-06 21:16:20 -0700

    Update app.java

--- app.java
+++ app.java
@@ -1,5 +1,14 @@
-hey
-hi ra
-ela unnavu
-eroje github 
-start chesam
+public class AsciiValue {
+
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
+Output
